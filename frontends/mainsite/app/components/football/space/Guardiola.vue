7909250eeed4c73b53a7dc611bfa7c17f04836b3
Zadigo <pendenquejohn@gmail.com>
2026-07-19 12:19:23 +0200

    Initial commit

--- frontends/mainsite/app/components/football/space/Guardiola.vue
+++ frontends/mainsite/app/components/football/space/Guardiola.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-full grid grid-cols-4 grid-rows-5 gap-2 rounded-3xl overflow-hidden text-3xl font-bold pointer-events-none">
+  <div class="w-full h-full grid grid-cols-4 grid-rows-5 gap-2 rounded-3xl overflow-hidden text-3xl font-bold pointer-events-none text-vertical-space-50">
     <div :class="glassmorphismTheme" class="text-vertical-space-50 flex items-center justify-center">
       Aile
     </div>
@@ -17,7 +17,7 @@
     </div>
 
     <!-- Centre -->
-    <div class="grid col-span-2 row-span-3 bg-vertical-space-100 relative overflow-hidden">
+    <!-- <div class="grid col-span-2 row-span-3 bg-vertical-space-100 relative overflow-hidden">
       <div class="grid grid-cols-12 grid-rows-1 gap-2">
         <div class="bg-vertical-space-700 text-vertical-space-50 flex items-center justify-center col-span-3">
           <div class="-rotate-90 min-w-50">
@@ -25,9 +25,7 @@
           </div>
         </div>
         
-        <!-- Sub-centre -->
         <div class="bg-vertical-space-100 text-vertical-space-50 h-full col-span-6">
-          <!-- Sub-center -->
           <div class="grid grid-cols-1 grid-rows-2 gap-4 w-full h-full">
             <div class="flex items-center justify-center bg-vertical-space-800">
               <div class="-rotate-90">
@@ -46,6 +44,30 @@
         <div class="bg-vertical-space-700 text-vertical-space-50 flex items-center justify-center h-full col-span-3">
           <div class="-rotate-90 min-w-50">
             Demi-espace
+          </div>
+        </div>
+      </div>
+    </div> -->
+
+    <div class="grid col-span-2 row-span-3 bg-vertical-space-100 relative overflow-hidden">
+      <div class="grid grid-rows-2 gap-y-1">
+        <div v-for="i in 2" :key="i" data-id="center" class="grid grid-cols-12 gap-2">
+          <div class="flex items-center justify-center bg-vertical-space-800 col-span-3">
+            <div class="-rotate-90">
+              1/2
+            </div>
+          </div>
+
+          <div class="flex items-center justify-center bg-vertical-space-800 col-span-6">
+            <div class="-rotate-90">
+              Centre
+            </div>
+          </div>
+
+          <div class="flex items-center justify-center bg-vertical-space-800 col-span-3">
+            <div class="-rotate-90">
+              1/2
+            </div>
           </div>
         </div>
       </div>
